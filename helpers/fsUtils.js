337eda851889f53cfde8791736fb94385581e2b4
newprice247 <newprice247@gmail.com>
2023-09-18 11:48:58 -0400

    m

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -7,14 +7,14 @@ const readFromFilePromise = util.promisify(fs.readFile);
 const writeToFilePromise = util.promisify(fs.writeFile)
 //Function that takes the file path and some kind of content as arguments, then writes the content to the file specified
 const writeToFile = (file, content) =>
-    writeToFilePromise(file, JSON.stringify(content), (err) =>
+    fs.writeFile(file, JSON.stringify(content), (err) =>
     err ? console.log(err) : console.log(`\nData written to ${file}${content}`)
 );
 
 //Handles 'DELETE' requests
 //Function that reads and pulls data from a specified file, parses the 'data' found in that file, filters out the specified 'content', then rewrites the leftover 'data' back into the original file.
 const readAndDelete = (content, file) => {
-    readFromFilePromise(file, 'utf-8', (err, data) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
         console.log(content)
         if (err) {
             console.error(err)
@@ -30,7 +30,7 @@ const readAndDelete = (content, file) => {
 //Handles 'POST' requests
 //Function that reads and pulls data from a specified file, parses the data found in the file, then adds the specified content into the array of data and rewrites the data back into it's original file.
 const readAndAppend = (content, file) => {
-    readFromFilePromise(file, 'utf-8', (err, data) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
         console.log(content)
         if (err) {
             console.error(err)
