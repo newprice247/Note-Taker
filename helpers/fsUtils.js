ecceef89bafed7be6260a5f6c0e7b063f3ac22e4
newprice247 <newprice247@gmail.com>
2023-09-18 09:58:45 -0400

    added comments

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,15 +1,17 @@
-
+//Imports node module for managing file systems
 const fs = require('fs');
+//Imports node module for altering utility behaviors
 const util = require('util');
-
+//Alters the behavior of the 'readFile' and 'writeFile' methods of the 'fs' module
 const readFromFilePromise = util.promisify(fs.readFile);
 const writeToFilePromise = util.promisify(fs.writeFile)
-
+//Function that takes the file path and some kind of content as arguments, then writes the content to the file specified
 const  writeToFile= (file, content) =>
     writeToFilePromise(file, JSON.stringify(content), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${file}`)
     );
-
+//Function that reads and pulls data from a specified file, parses the 'data' found in that file, filters out the specified 'content', then rewrites the leftover 'data' back into the original file.
+//Handles 'DELETE' requests
 const readAndDelete = (content, file) => {
     readFromFilePromise(file, 'utf-8', (err, data) => {
         console.log(content)
@@ -22,7 +24,8 @@ const readAndDelete = (content, file) => {
         }
     });
 }
-
+//Function that reads and pulls data from a specified file, parses the data found in the file, then adds the specified content into the array of data and rewrites the data back into it's original file.
+//Handles 'POST' requests
 const readAndAppend = (content, file) => {
     readFromFilePromise(file, 'utf-8', (err, data) => {
         if (err) {
@@ -35,4 +38,5 @@ const readAndAppend = (content, file) => {
     });
 };
 
+//Exports the functions that handle 'DELETE' and 'POST' requests
 module.exports = { readAndAppend, readAndDelete };
